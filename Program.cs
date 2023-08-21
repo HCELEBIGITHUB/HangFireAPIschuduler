326a412eb43c4b896bd79692b3597e14921a33c3
HCELEBIGITHUB <hidir_celebi1981@hotmail.com>
2023-08-22 01:20:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using Hangfire;
+using Hangfire.SqlServer;
+using HangfireBasicAuthenticationFilter;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+IConfiguration _configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .Build();
+
+
+builder.Services.AddHangfire(configuration => configuration
+       .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+       .UseSimpleAssemblyNameTypeSerializer()
+       .UseRecommendedSerializerSettings()
+       .UseSqlServerStorage(builder.Configuration.GetConnectionString("HangfireConnection"), new SqlServerStorageOptions
+       {
+           CommandBatchMaxTimeout = TimeSpan.FromMinutes(5),
+           SlidingInvisibilityTimeout = TimeSpan.FromMinutes(5),
+           QueuePollInterval = TimeSpan.Zero,
+           UseRecommendedIsolationLevel = true,
+           DisableGlobalLocks = true
+       }));
+builder.Services.AddHangfireServer();
+
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+//app.UseHangfireDashboard("/dashboard");
+app.UseHangfireDashboard("/job", new DashboardOptions
+{
+    Authorization = new[]
+    {
+        new HangfireCustomBasicAuthenticationFilter
+        {
+         User = _configuration.GetSection("HangfireSettings:UserName").Value,
+         Pass = _configuration.GetSection("HangfireSettings:Password").Value
+    }
+    }
+});
+
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
